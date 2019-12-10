41120de0adeec6f4552f4d3a3acd5ffab9b9b0f7
d0dders <dodders@outlook.com>
2019-12-10 21:14:46 +0000

    Refactored createLi and event handler

--- app.js
+++ app.js
@@ -58,46 +58,51 @@ document.addEventListener('DOMContentLoaded', () => {
             const button = e.target;
             const li = button.parentNode;
             const ul = li.parentNode;
-            if (button.textContent === "remove") {
-                ul.removeChild(li);
-            } else if (button.textContent === "edit") {
-                const span = li.firstElementChild;
-                const input = document.createElement('input');
-                input.type = "text";
-                input.value = span.textContent;
-                li.insertBefore(input, span);
-                li.removeChild(span);
-                button.textContent = "save";
-            } else if (button.textContent === "save") {
-                const input = li.firstElementChild;
-                const span = document.createElement('span');
-                span.textContent = input.value;
-                li.insertBefore(span, input);
-                li.removeChild(input);
-                button.textContent = "edit";
-            }
+            const action = button.textContent;
+            const nameActions = {
+                remove: () => {
+                    ul.removeChild(li);
+                },
+                edit: () => {
+                    const span = li.firstElementChild;
+                    const input = document.createElement('input');
+                    input.type = "text";
+                    input.value = span.textContent;
+                    li.insertBefore(input, span);
+                    li.removeChild(span);
+                    button.textContent = "save";
+                },
+                save: () => {
+                    const input = li.firstElementChild;
+                    const span = document.createElement('span');
+                    span.textContent = input.value;
+                    li.insertBefore(span, input);
+                    li.removeChild(input);
+                    button.textContent = "edit";
+                }
+            };
+
+            nameActions[action]();
         }
     });
 
     function createLi(text) {
+        function createElement(elementName, property, value) {
+            const element = document.createElement(elementName);
+            element[property] = value;
+            return element;
+        }
+        function appendToLi(elementName, property, value) {
+            const element = createElement(elementName, property, value);
+            li.appendChild(element);
+            return element;
+        }
         const li = document.createElement('li');
-        const span = document.createElement('span');
-        span.textContent = text;
-        li.appendChild(span);
-        const label = document.createElement('label');
-        label.textContent = "Confirmed";
-        const checkbox = document.createElement('input');
-        checkbox.type = "checkbox";
-        label.appendChild(checkbox);
-        li.appendChild(label);
-
-        const editButton = document.createElement('button');
-        editButton.textContent = "edit";
-        li.appendChild(editButton);
-
-        const removeButton = document.createElement('button');
-        removeButton.textContent = "remove";
-        li.appendChild(removeButton);
+        appendToLi('span', 'textContent', text);
+        appendToLi('label', 'textContent', "Confirmed")
+            .appendChild(createElement('input', 'type', "checkbox"));
+        appendToLi('button', 'textContent', "edit");
+        appendToLi('button', 'textContent', 'remove');
         return li;
     }
 });
